b3b6a677e3d677654c1ec78b3ac556cd4b14b661
Caranille <mycaranille@gmail.com>
2017-06-28 21:49:30 +0200

    fix

--- modules/contact/index.php
+++ modules/contact/index.php
@@ -1,6 +1,6 @@
 <?php require_once("../../html/header.php"); ?>
 
 <h1>Me contacter</h1>
-Pour toutes questions/suggestion et soumissions de bogues merci de me contacter à cette adresse Email: mycaranille@gmail.com
+Pour toutes questions/suggestion et soumissions de bogues merci de me contacter à cette adresse Email: jeremy@caranille.com
 
 <?php require_once("../../html/footer.php"); ?>
