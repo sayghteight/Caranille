cb5f480f1e079860c387780644d8c7dd6440a19f
Caranille <mycaranille@gmail.com>
2017-07-09 13:45:02 +0200

    fix

--- administration/configuration/index.php
+++ administration/configuration/index.php
@@ -5,10 +5,11 @@ require_once("../html/header.php");
 if (empty($_SESSION)) { exit(header("Location: ../../index.php")); }
 //Si le joueur n'a pas les droits administrateurs (Accès 2) on le redirige vers l'accueil
 if ($accountAccess < 2) { exit(header("Location: ../../index.php")); }
-        
+
+//On fait une requête dans la base de donnée pour récupérer les informations du jeu
 $configurationQuery = $bdd->query("SELECT * FROM car_configuration");
 
-//On fait une boucle pour récupérer toutes les information
+//On fait une boucle sur le ou les résultats obtenu pour récupérer les informations
 while ($configuration = $configurationQuery->fetch())
 {
     //On récupère les informations du jeu
@@ -26,8 +27,8 @@ $configurationQuery->closeCursor();
 <form method="POST" action="editConfiguration.php">
     Nom du jeu : <input type="text" name="adminGameName" class="form-control" placeholder="Nom du jeu" value="<?php echo $adminGameName; ?>" required>
     Présentation : <br> <textarea class="form-control" name="adminGamePresentation" id="adminGamePresentation" rows="3" required><?php echo $adminGamePresentation; ?></textarea>
-    Base expérience (Base * niveau = expérience demandée pour monter de niveau) : <input type="text" name="adminGameExperience" class="form-control" placeholder="Base expérience" value="<?php echo $adminGameExperience; ?>" required>
-    PC par niveau : <input type="text" name="adminGameSkillPoint" class="form-control" placeholder="PC par niveau" value="<?php echo $adminGameSkillPoint; ?>" required>
+    Base expérience (Base * niveau = expérience demandée pour monter de niveau) : <input type="number" name="adminGameExperience" class="form-control" placeholder="Base expérience" value="<?php echo $adminGameExperience; ?>" required>
+    PC par niveau : <input type="number" name="adminGameSkillPoint" class="form-control" placeholder="PC par niveau" value="<?php echo $adminGameSkillPoint; ?>" required>
     Status du jeu : <select name="adminGameAccess" class="form-control">
     
         <?php
@@ -35,15 +36,19 @@ $configurationQuery->closeCursor();
         {
             case "Opened":
                 ?>
-                <option selected="selected" value="Opened">Ouvert</option>
-                <option value="Closed">Fermé</option>
+
+                    <option selected="selected" value="Opened">Ouvert</option>
+                    <option value="Closed">Fermé</option>
+
                 <?php
             break;
     
             case "Closed":
                 ?>
-                <option value="Opened">Ouvert</option>
-                <option selected="selected" value="Closed">Fermé</option>
+
+                    <option value="Opened">Ouvert</option>
+                    <option selected="selected" value="Closed">Fermé</option>
+
                 <?php
             break;
         }
