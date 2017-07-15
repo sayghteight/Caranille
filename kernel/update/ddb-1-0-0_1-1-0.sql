ee36c6e74c79a5106811113c822c5899c31f1936
Caranille <mycaranille@gmail.com>
2017-07-15 14:07:36 +0200

    fix

--- kernel/update/ddb-1-0-0_1-1-0.sql
+++ kernel/update/ddb-1-0-0_1-1-0.sql
@@ -2,11 +2,12 @@
 -- Mise à jour de la base de donnée de Caranille 1.0.0 à 1.1.0
 --
 
-CREATE TABLE IF NOT EXISTS `Caranille_Chat` 
+CREATE TABLE IF NOT EXISTS `car_chat` 
 (
-  `Chat_Message_ID` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `Chat_Character_ID` int(5) NOT NULL,
-  `Chat_Message` text NOT NULL
+  `chatMessageId` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `chatCharacterId` int(5) NOT NULL,
+  `chatDateTime` datetime NOT NULL,
+  `chatMessage` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `car_private_conversation` 
