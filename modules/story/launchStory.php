5a64594ce94c4d8ec4f97dff2889befda629e9e5
Caranille <mycaranille@gmail.com>
2017-07-16 13:19:14 +0200

    Update launchStory.php

--- modules/story/launchStory.php
+++ modules/story/launchStory.php
@@ -2,8 +2,6 @@
 
 //S'il n'y a aucune session c'est que le joueur n'est pas connecté alors on le redirige vers l'accueil
 if (empty($_SESSION)) { exit(header("Location: ../../index.php")); }
-//Si le joueur n'est pas dans une ville on le redirige vers la carte du monde
-if ($characterTownId == 0) { exit(header("Location: ../../modules/map/index.php")); }
 //S'il y a actuellement un combat on redirige le joueur vers le module battle
 if ($battleRow > 0) { exit(header("Location: ../../modules/battle/index.php")); }
 
