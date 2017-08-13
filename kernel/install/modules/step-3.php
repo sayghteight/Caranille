8595049427ec79b31717ebfad4848329d0f3b6f0
Caranille <mycaranille@gmail.com>
2017-08-13 12:01:44 +0200

    fix

--- kernel/install/modules/step-3.php
+++ kernel/install/modules/step-3.php
@@ -11,7 +11,7 @@
     fwrite($openSql, "
     <?php
     //Version of Caranille
-    \$version = \"1.5.1\";
+    \$version = \"1.5.2\";
     \$dsn = 'mysql:dbname=$databaseName;host=$databaseHost';
     \$user = '$databaseUser';
     \$password = '$databasePassword';
