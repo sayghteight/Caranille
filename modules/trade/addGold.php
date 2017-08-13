f2d56fb2ed118b4f8ab3a06a0d6d97a8f7b833cf
Caranille <mycaranille@gmail.com>
2017-08-13 12:00:31 +0200

    fix

--- modules/trade/addGold.php
+++ modules/trade/addGold.php
@@ -49,6 +49,15 @@ if (isset($_POST['tradeId'])
             'characterId' => $characterId,
             'tradeId' => $tradeId));
             $updateGoldTrade->closeCursor();
+            
+            //On met l'échange à jour
+            $updateTrade = $bdd->prepare("UPDATE car_trades SET
+            tradeCharacterOneTradeAccepted = 'No',
+            tradeCharacterTwoTradeAccepted = 'No'
+            WHERE tradeId = :tradeId");
+            $updateTrade->execute(array(
+            'tradeId' => $tradeId));
+            $updateTrade->closeCursor(); 
                         
             //Si le joueur est revenu sur cette page c'est qu'il souhaite modifier l'argent de l'échange, on lui rend donc l'argent qu'il avait mit avant
             $updateGoldTrade = $bdd->prepare("UPDATE car_characters SET
